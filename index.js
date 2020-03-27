0f39501c4fed7529b26e8591392671b2a1bf4569
Eucharia Ifunanya <60736117+ifuu16@users.noreply.github.com>
2020-03-27 17:26:57 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+ let name="IFUNANYA EUCHARIA";
+console.log("my name is: " + name);
+
+
+const data = [];
+
+for (let i=0; i<=200; i++) {
+
+  if (i%1===0) {
+
+    data.push(i);
+
+  }
+
+}
+
+console.log(data);
+
+
+
+
